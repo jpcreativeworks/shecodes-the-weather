3b8db374eeecb0c1eba61cfd7ec6bf6a3114f0a0
Jenn <jp.graphics.011@gmail.com>
2025-03-08 04:57:44 -0700

    js weather details api call added

--- src/script.js
+++ src/script.js
@@ -2,9 +2,18 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityName = document.querySelector("#city-name");
+  let weatherDescription = document.querySelector("#description");
+  let weatherHumidity = document.querySelector("#humidity");
+  let weatherWind = document.querySelector("#wind");
+  let weatherWindSpeed = response.data.wind.speed;
+  console.log(response.data);
+
   cityName.innerHTML = response.data.city;
+  weatherWind.innerHTML = Math.round(weatherWindSpeed);
+  weatherHumidity.innerHTML = response.data.temperature.humidity;
+  weatherDescription.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
-  console.log(response.data.temperature.current);
+  // console.log(response.data.temperature.current);
 }
 
 function searchCity(city) {
